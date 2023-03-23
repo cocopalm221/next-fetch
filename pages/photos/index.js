c97a56fd3f68025d581b3519cfdfc78b43da7712
h.young <cocotree221@gmail.com>
2023-03-23 11:58:28 +0900

    data fetching

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -1,26 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import HeaderInfo from "../../components/HeaderInfo";
 import PhotosStyle from "../../styles/Photos.module.css";
 
 const photos = ({ cats }) => {
   return (
-    <>
+    <div className={PhotosStyle.photosWrap}>
       <HeaderInfo title="Cats World - photo" />
       <ul className={PhotosStyle.photos}>
         {cats.map((cat) => (
           <li key={cat.name}>
-            <Image
-              src={cat.image.url}
-              width={160}
-              height={130}
-              alt={cat.name}
-            />
-            <span>{cat.name}</span>
+            <Link href={`photos/${cat.id}`}>
+              <a>
+                <Image
+                  src={cat.image.url}
+                  width={160}
+                  height={130}
+                  alt={cat.name}
+                />
+                <span>{cat.name}</span>
+              </a>
+            </Link>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
